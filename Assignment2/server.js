1b553c18cfd86882d81a8f639c9b059ec92579ae
Amanda Adkins <aaadkins@wpi.edu>
2015-11-12 00:56:29 -0500

    Get cgi-y javascript working for searching movies

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -6,6 +6,8 @@ var movies = ['jaws', 'jaws1', 'Monty Python'];
 var app = express();
 var port = process.env.PORT || 3000;
 
+var fs = require('fs');
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/index', function(req, res) {
@@ -15,20 +17,39 @@ app.get('/index', function(req, res) {
   } else if (typeof query.add != 'undefined') {
     addMovie(query.add);
     res.sendFile(path.join(__dirname, 'public/index.html')); //)('chair, table, tacos, funyuns, apple');
+  } else {
+    res.sendFile(path.join(__dirname, 'public/index.html')); //)('chair, table, tacos, funyuns, apple');
   }
- else {
-  res.sendFile(path.join(__dirname, 'public/index.html')); //)('chair, table, tacos, funyuns, apple');
-}
 });
 
 function queryMovie(res, searchQuery) {
   matchingMovies = movies.filter(checkNameInTitle(searchQuery));
   if (matchingMovies.length != 0) {
     console.log(matchingMovies);
-    res.send(matchingMovies.join(", "));
+
+    var fileStream = fs.createReadStream(path.join(__dirname, 'public/index.html'));
+
+    var htmlString;
+
+    fileStream.on('data', function(data) {
+      console.log(typeof data);
+      htmlString = data.toString();
+      //res.write(data);
+    });
+
+    fileStream.on('end', function() {
+    //  res.end();
+        moviesString = generateHTMLTableRowsBasedOnMovies(matchingMovies);
+        console.log(moviesString);
+        resultHTML = modifyTableContentsInFile(htmlString, moviesString);
+        console.log(resultHTML);
+        res.send(resultHTML);
+    })
+
+
   } else {
-    res.send("No movies containing " + searchQuery + " in title.")
-    res.sendFile(path.join(__dirname, 'public/index.html')); //)('chair, table, tacos, funyuns, apple');
+    res.send("No movies containing " + searchQuery + " in title.");
+    //res.sendFile(path.join(__dirname, 'public/index.html')); //)('chair, table, tacos, funyuns, apple');
   }
 }
 
@@ -60,4 +81,18 @@ function checkNameInTitle(name) {
   return function(obj) {
     return obj.indexOf(name) >= 0;
   }
+}
+
+function generateHTMLTableRowsBasedOnMovies(moviesToSerialize) {
+  var i;
+  var tableString="";
+  for (i = 0; i < moviesToSerialize.length; i++) {
+    tableString += "<tr><td>" + moviesToSerialize[i] + "</td></tr>\n";
+  }
+  return tableString;
+}
+
+function modifyTableContentsInFile(originalString, replacementText) {
+  console.log(typeof originalString);
+  return originalString.replace('<table id="resultsTable">', '<table id="resultsTable">' + replacementText);
 }
