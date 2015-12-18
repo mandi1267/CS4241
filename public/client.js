044dee1656523eefa0663cf7bf12feb191aed16b
Amanda Adkins <aaadkins@wpi.edu>
2015-12-17 19:00:06 -0500

    Cookies not working, seeing if localhost

--- public/client.js
+++ public/client.js
@@ -1,11 +1,14 @@
 /* Amanda Adkins, website: http://aaadkins-assignment7.herokuapp.com/ */
 
-maxYakID  = 0;
+maxUserID = 0;
+maxYakID = 0;
 
 userUppedYaks = [];
 userDownedYaks = [];
 
 allYaks = [];
+
+yaksList = [];
 
 function onPageLoad() {
   // get yaks from server
@@ -13,7 +16,14 @@ function onPageLoad() {
   displayAllYaks(allYaks);
   // check if userID in localstorage
 
+  form = document.getElementById('yakAdder');
+  form.addEventListener('submit', function(event) {
+    console.log('submitting');
+    addYak();
+    event.preventDefault();
+  });
 }
+
 
 
 function Yak(timestamp, yakMsg, gpsLoc, replies, votes, userID, yakID, vote) {
@@ -34,23 +44,30 @@ function makeNewYak(yakMsg, userID) {
   maxYakID += 1;
   yakID = maxYakID;
   if (navigator.geolocation) {
-    gpsLoc = navigator.geolocation.getCurrentPosition();
+    navigator.geolocation.getCurrentPosition(function(gpsLoc) {
+      newYak = Yak(timestamp, yakMsg, gpsLoc, replies, votes, userID, yakID, 'noVote');
+    });
     console.log(gpsLoc);
   } else {
     // don't know what to do here
     gpsLoc = "";
+    newYak = Yak(timestamp, yakMsg, gpsLoc, replies, votes, userID, yakID, 'noVote');
+    return newYak;
   }
-  newYak = Yak(timestamp, yakMsg, gpsLoc, replies, votes, userID, yakID, 'noVote');
-  return newYak;
+
 }
 
 function displayAllYaks(yaksList) {
-  yakHTML = "";
-  yaksList.forEach(function(p, i) {
-    yakHTML += yakTemplate(p);
-  });
-  yakBody = document.getElementById('yakBody');
-  yakBody.innerHTML = yakHTML;
+  if (yaksList.length === 0) {
+    yakBody.innerHTML = 'No yaks to display';
+  } else {
+    yakHTML = "";
+    yaksList.forEach(function(p, i) {
+      yakHTML += yakTemplate(p);
+    });
+    yakBody = document.getElementById('yakBody');
+    yakBody.innerHTML = yakHTML;
+  }
 }
 
 function didUserUpYak(yakID) {
@@ -65,4 +82,78 @@ function didUserDownYak(yakID) {
     return true;
   }
   return false;
+}
+
+function getMaxUserId(yaksList) {
+  maxUserID = 0;
+  for (i = 0; i < yaksList.length; i++) {
+    if (yaksList[i].userID > maxUserID) {
+      maxUserID = yaksList[i].userID;
+    }
+  }
+}
+
+function getMaxYakID(yaksList) {
+  maxUserID = 0;
+  for (i = 0; i < yaksList.length; i++) {
+    if (yaksList[i].userID > maxUserID) {
+      maxUserID = yaksList[i].userID;
+    }
+  }
+
+}
+
+function gpsInRange(glsLoc1, gpsLoc2) {
+
+}
+
+function addYak() {
+  yakMsgBox = document.getElementById('yakBox');
+  yakMsg = yakMsgBox.value;
+
+  userId = getUserID();
+  console.log(userId);
+
+
+  getMaxUserID(function(response) {
+    maxID = response;
+    if (userId < 0) {
+      userId = maxID
+      console.log('maxID');
+      console.log(maxID);
+      console.log("settign cookie");
+      console.log(userId.toString());
+      document.cookie= 'userID='+userId.toString();
+    }
+    // make new yak
+
+    displayAllYaks(yaksList);
+  });
+}
+
+function getMaxUserID(callback) {
+  handleXMLHTTPGet('/getMaxUserID', 'none', callback);
+}
+
+function getUserID() {
+  console.log("Cookie");
+  console.log(document.cookie.userID)
+  userID = document.cookie.replace(/(?:(?:^|.*;\s*)userID\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  console.log(userID);
+
+  if (userID === "") {
+    console.log('no user ID');
+    return -1;
+  } else {
+    console.log('usserID ' + userID);
+    return parseInt(userID);
+  }
+}
+
+function sortYaksByTime(yaksList) {
+
+}
+
+function filterYaksByGPSLoc() {
+
 }
--- server.js
+++ server.js
@@ -20,9 +20,39 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/getMaxUserID', function(req, res) {
+  res.send(getMaxUserID());
+});
+
+app.get('/getYaksList', function(req, res) {
+  readAllYaks(function(yaksList) {
+    res.send(JSON.stringify(yaksList));
+  });
+});
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 app.listen(port, function() {
   console.log('App is listening on port ' + port);
 });
+
+function getMaxUserID() {
+  return (0).toString();
+}
+
+
+function readAllYaks(callbackFunc) {
+  var fileStream = fs.createReadStream(path.join(__dirname, 'public/yaksFile.txt'));
+  var articlesString = "";
+
+  fileStream.on('data', function(data) {
+    articlesString += data.toString();
+  });
+
+  fileStream.on('end', function() {
+    //console.log("hi");
+    //callbackFunc(articlesString);
+    callbackFunc(JSON.parse(articlesString));
+  });
+}
